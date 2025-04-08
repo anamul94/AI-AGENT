b5e21182cf87340f45f367ad92bd08b30f456507
anam <anamul.ice14@gmail.com>
2025-04-08 21:54:46 +0600

    YT Summarizer agent

--- YT-SUMMARIZER/agnets.py
+++ YT-SUMMARIZER/agnets.py
@@ -8,7 +8,7 @@ load_dotenv()
 from langchain_groq import ChatGroq
 
 llm = ChatGroq(
-    model="llama-3.1-8b-instant",
+    model="gemma2-9b-it",
     temperature=0,
     max_tokens=None,
     timeout=None,
